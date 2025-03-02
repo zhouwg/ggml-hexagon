f46cc3842d676c035f2e4189a44b5afc5c8ed302
zhouwg <zhouwg2000@gmail.com>
2025-03-02 08:37:25 +0800

    ggml-qnn: merge UT code and corresponding script from local dev branch to make workflow easily

--- scripts/build-run-android-minimal.sh
+++ scripts/build-run-android-minimal.sh
@@ -129,37 +129,32 @@ function build_ggml_qnn()
 }
 
 
-function prepare_run_on_phone()
+function run_llamacli()
 {
-    if [ $# != 1 ]; then
-        print "invalid param"
-        return
-    fi
-    program=$1
-
     check_qnn_libs
 
     if [ -f ./out/android/bin/libggml-qnn.so ]; then
         adb push ./out/android/bin/*.so ${REMOTE_PATH}/
     fi
-    adb push ./out/android/bin/${program} ${REMOTE_PATH}/
-    adb shell chmod +x ${REMOTE_PATH}/${program}
-}
-
-function run_llamacli()
-{
-    prepare_run_on_phone llama-cli
+    adb push ./out/android/bin/llama-cli ${REMOTE_PATH}/
+    adb shell chmod +x ${REMOTE_PATH}/llama-cli
 
     adb shell "cd ${REMOTE_PATH} \
                && export LD_LIBRARY_PATH=${REMOTE_PATH} \
-               && ${REMOTE_PATH}/llama-cli -mg ${qnnbackend} -no-ncv -m ${GGUF_MODEL_NAME} -p \"introduce the movie Once Upon a Time in America briefly.\n\""
+               && ${REMOTE_PATH}/llama-cli -mg ${qnnbackend} -no-cnv -m ${GGUF_MODEL_NAME} -p \"introduce the movie Once Upon a Time in America briefly.\n\""
 
 }
 
 
 function run_llamabench()
 {
-    prepare_run_on_phone llama-bench
+    check_qnn_libs
+
+    if [ -f ./out/android/bin/libggml-qnn.so ]; then
+        adb push ./out/android/bin/*.so ${REMOTE_PATH}/
+    fi
+    adb push ./out/android/bin/llama-bench ${REMOTE_PATH}/
+    adb shell chmod +x ${REMOTE_PATH}/llama-bench
 
     adb shell "cd ${REMOTE_PATH} \
                && export LD_LIBRARY_PATH=${REMOTE_PATH} \
@@ -170,41 +165,17 @@ function run_llamabench()
 
 function run_test-backend-ops()
 {
-    prepare_run_on_phone test-backend-ops
+    check_qnn_libs
+
+    if [ -f ./out/android/bin/libggml-qnn.so ]; then
+        adb push ./out/android/bin/*.so ${REMOTE_PATH}/
+    fi
+    adb push ./out/android/bin/test-backend-ops ${REMOTE_PATH}/
+    adb shell chmod +x ${REMOTE_PATH}/test-backend-ops
 
     adb shell "cd ${REMOTE_PATH} \
                && export LD_LIBRARY_PATH=${REMOTE_PATH} \
                && ${REMOTE_PATH}/test-backend-ops test"
-
-}
-
-function run_ut_add()
-{
-    prepare_run_on_phone ggml-qnn-ut
-
-    adb shell "cd ${REMOTE_PATH} \
-               && export LD_LIBRARY_PATH=${REMOTE_PATH} \
-               && ${REMOTE_PATH}/ggml-qnn-ut -t GGML_OP_ADD -b $qnnbackend"
-
-}
-
-function run_ut_mulmat()
-{
-    prepare_run_on_phone ggml-qnn-ut
-
-    adb shell "cd ${REMOTE_PATH} \
-               && export LD_LIBRARY_PATH=${REMOTE_PATH} \
-               && ${REMOTE_PATH}/ggml-qnn-ut -t GGML_OP_MUL_MAT -b $qnnbackend"
-
-}
-
-function run_ut_mul()
-{
-    prepare_run_on_phone ggml-qnn-ut
-
-    adb shell "cd ${REMOTE_PATH} \
-               && export LD_LIBRARY_PATH=${REMOTE_PATH} \
-               && ${REMOTE_PATH}/ggml-qnn-ut -t GGML_OP_MUL -b $qnnbackend"
 
 }
 
@@ -215,9 +186,6 @@ function show_usage()
     echo "  $0 build"
     echo "  $0 updateqnnlib"
     echo "  $0 run_testop"
-    echo "  $0 run_ut_add       0 (QNN_CPU) / 1 (QNN_GPU) / 2 (QNN_NPU) / 3 (ggml)"
-    echo "  $0 run_ut_mulmat    0 (QNN_CPU) / 1 (QNN_GPU) / 2 (QNN_NPU) / 3 (ggml)"
-    echo "  $0 run_ut_mul       0 (QNN_CPU) / 1 (QNN_GPU) / 2 (QNN_NPU) / 3 (ggml)"
     echo "  $0 run_llamacli     0 (QNN_CPU) / 1 (QNN_GPU) / 2 (QNN_NPU) / 3 (ggml)"
     echo "  $0 run_llamabench   0 (QNN_CPU) / 1 (QNN_GPU) / 2 (QNN_NPU) / 3 (ggml)"
     echo -e "\n\n\n"
@@ -245,7 +213,6 @@ elif [ $# == 1 ]; then
     elif [ "$1" == "run_testop" ]; then
         run_test-backend-ops
         exit 0
-
     elif [ "$1" == "updateqnnlib" ]; then
         update_qnn_libs
         exit 0
@@ -265,15 +232,6 @@ elif [ $# == 2 ]; then
         exit 0
     elif [ "$1" == "run_llamabench" ]; then
         run_llamabench
-        exit 0
-    elif [ "$1" == "run_ut_add" ]; then
-        run_ut_add
-        exit 0
-    elif [ "$1" == "run_ut_mulmat" ]; then
-        run_ut_mulmat
-        exit 0
-    elif [ "$1" == "run_ut_mul" ]; then
-        run_ut_mul
         exit 0
     fi
 else
