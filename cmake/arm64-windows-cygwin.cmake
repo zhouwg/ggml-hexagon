3735b793edbc8511dbb303e42523928d5ff59880
zhouwg <zhouwg2000@gmail.com>
2025-03-08 08:09:02 +0800

    ggml-qnn: WoA(Windows on ARM) -- step6

--- cmake/arm64-windows-cygwin.cmake
+++ cmake/arm64-windows-cygwin.cmake
@@ -1,7 +1,7 @@
-set( CMAKE_SYSTEM_NAME Windows )
+set( CMAKE_SYSTEM_NAME CYGWIN)
 set( CMAKE_SYSTEM_PROCESSOR arm64 )
 
-set( target arm64-pc-windows-msvc )
+set( target aarch64-w64-cygwin)
 
 set( CMAKE_C_COMPILER    clang )
 set( CMAKE_CXX_COMPILER  clang++ )
@@ -9,8 +9,8 @@ set( CMAKE_CXX_COMPILER  clang++ )
 set( CMAKE_C_COMPILER_TARGET   ${target} )
 set( CMAKE_CXX_COMPILER_TARGET ${target} )
 
-#set( arch_c_flags "-march=armv8.7-a -fvectorize -ffp-model=fast -fno-finite-math-only" )
-#set( warn_c_flags "-Wno-format -Wno-unused-variable -Wno-unused-function -Wno-gnu-zero-variadic-macro-arguments" )
+set( arch_c_flags "-march=armv8.7-a -fvectorize -ffp-model=fast -fno-finite-math-only" )
+set( warn_c_flags "-Wno-format -Wno-unused-variable -Wno-unused-function -Wno-gnu-zero-variadic-macro-arguments" )
 
 set( CMAKE_C_FLAGS_INIT   "${arch_c_flags} ${warn_c_flags}" )
 set( CMAKE_CXX_FLAGS_INIT "${arch_c_flags} ${warn_c_flags}" )
